e9c62f40fb01a5034ba3164c7428e808728bc2fd
jinishgupta <jinishgupta272@gmail.co>
2025-05-08 14:55:38 +0530

    trying to deploy website

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import memoryIcon from './assets/memory-icon.svg'
+
+// Set favicon
+const link = document.createElement('link')
+link.rel = 'icon'
+link.type = 'image/svg+xml'
+link.href = memoryIcon
+document.head.appendChild(link)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
